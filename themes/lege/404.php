daf9c5e077bee276893e1c7b7bb4980add4a7e74
Elina Ontisvili <63250296+elinaontisvili@users.noreply.github.com>
2025-07-09 13:15:23 +0300

    Refactor 404 page layout and enhance footer structure with subscribe shortcode

--- themes/lege/404.php
+++ themes/lege/404.php
@@ -6,55 +6,18 @@
  *
  * @package Lege
  */
-
 get_header();
+
 ?>
-
-	<main id="primary" class="site-main">
-
-		<section class="error-404 not-found">
-			<header class="page-header">
-				<h1 class="page-title"><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'lege' ); ?></h1>
-			</header><!-- .page-header -->
-
-			<div class="page-content">
-				<p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'lege' ); ?></p>
-
-					<?php
-					get_search_form();
-
-					the_widget( 'WP_Widget_Recent_Posts' );
-					?>
-
-					<div class="widget widget_categories">
-						<h2 class="widget-title"><?php esc_html_e( 'Most Used Categories', 'lege' ); ?></h2>
-						<ul>
-							<?php
-							wp_list_categories(
-								array(
-									'orderby'    => 'count',
-									'order'      => 'DESC',
-									'show_count' => 1,
-									'title_li'   => '',
-									'number'     => 10,
-								)
-							);
-							?>
-						</ul>
-					</div><!-- .widget -->
-
-					<?php
-					/* translators: %1$s: smiley */
-					$lege_archive_content = '<p>' . sprintf( esc_html__( 'Try looking in the monthly archives. %1$s', 'lege' ), convert_smilies( ':)' ) ) . '</p>';
-					the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$lege_archive_content" );
-
-					the_widget( 'WP_Widget_Tag_Cloud' );
-					?>
-
-			</div><!-- .page-content -->
-		</section><!-- .error-404 -->
-
-	</main><!-- #main -->
-
+<section class="inner-clients">
+	<div class="wrapper error-wrapper">
+		<h2 class="clients__title secondary-title error-title"><span><?php esc_html_e( 'Ошибка 404', 'lege' ); ?></span><br><?php esc_html_e( 'Страница не существует!' ); ?></h2>
+		<div class="error__box"> 
+			 <?php esc_html_e( 'Страница не найдена! Проверьте ссылку и обновите строку браузера.', 'lege' ); ?>
+		</div>
+		<a href="<?php echo esc_url(home_url()); ?>" class="error__btn btn"><?php esc_html_e( 'На главную', 'lege' ); ?></a>
+	</div>
+</section>
+	
 <?php
 get_footer();
