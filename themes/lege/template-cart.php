bdfe1c04edaf8e33d890c27782b2dc986068751a
Elina Ontisvili <63250296+elinaontisvili@users.noreply.github.com>
2025-09-26 13:36:20 +0300

    Add WooCommerce cart templates: implement cart, cart totals, cross-sells, shipping calculator, and proceed to checkout button

---
+++ themes/lege/template-cart.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Template Name: Шаблон: Корзина & Checkout
+ */
+
+get_header(); 
+?> 
+
+<section class="single cart pay"> 
+    <div class="wrapper"> 
+        <?php while (have_posts()) : the_post(); the_content(); endwhile; ?>
+    </div>
+</section>
+
+<?php 
+
+get_footer();
