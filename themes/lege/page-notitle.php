c6208f8a2e74f354b75da7c9c9d8febb435f7e6a
Elina Ontisvili <63250296+elinaontisvili@users.noreply.github.com>
2025-11-10 12:47:50 +0200

    Add page-notitle template and content part for displaying pages without titles

---
+++ themes/lege/page-notitle.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Template Name: Page Without Title
+ *
+ * @package Lege
+ */
+
+get_header();
+?>
+
+<section class="inner contacts">
+    <div class="wrapper">
+		<div id="primary" class="content-area"> 
+
+		<main id="primary" class="site-main">
+
+			<?php
+			while ( have_posts() ) :
+				the_post();
+
+				get_template_part( 'template-parts/content', 'page-notitle' );
+
+				// If comments are open or we have at least one comment, load up the comment template.
+				if ( comments_open() || get_comments_number() ) :
+					comments_template();
+				endif;
+
+			endwhile; // End of the loop.
+			?>
+
+		</main><!-- #main -->
+
+		</div> 
+	</div> 
+</section>
+
+<?php
+get_footer();
