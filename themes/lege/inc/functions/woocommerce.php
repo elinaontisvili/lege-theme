9bbea2eaf5111af09ec7fca9e9a2464358a3ff38
Elina Ontisvili <63250296+elinaontisvili@users.noreply.github.com>
2025-11-13 17:37:00 +0200

    feat(woocommerce): update badges and refine add-to-cart template
    - Removed unnecessary frontend attributes from add-to-cart link for cleaner HTML output.
    - Simplified add-to-cart markup and kept aria-describedby for accessibility.
    - Refactored lege_show_status() to use product ID and display custom badge only when meta is filled.
    - Limited custom badge to shop loop and removed from single product page.
    - Adjusted CSS to separate .discount and .new-item positioning, preventing overlap.

--- themes/lege/inc/functions/woocommerce.php
+++ themes/lege/inc/functions/woocommerce.php
@@ -1,13 +1,13 @@
 <?php 
 if(in_array('woocommerce/woocommerce.php', apply_filters('active_plugins', get_option('active_plugins')))) {
 
-    //Интеграция WooCommerce Шаблона
+    // Интеграция WooCommerce Шаблона
     function lege_add_woocommerce_support() {
         add_theme_support('woocommerce');
     }
     add_action('after_setup_theme', 'lege_add_woocommerce_support');
 
-    //Enable woo's built-in Ajax fragment refresh for cart count
+    // Enable woo's built-in Ajax fragment refresh for cart count
     add_filter('woocommerce_add_to_cart_fragments', 'lege_header_add_to_cart_fragment');
     function lege_header_add_to_cart_fragment( $fragments ) {
         global $woocommerce; 
@@ -20,7 +20,7 @@ if(in_array('woocommerce/woocommerce.php', apply_filters('active_plugins', get_o
         return $fragments; 
     }
 
-    //Убираем старый сайдбар и заменяем его персональным
+    // Убираем старый сайдбар и заменяем его персональным
     remove_action('woocommerce_sidebar', 'woocommerce_get_sidebar', 10);
 
     add_action('woocommerce_sidebar', function() {
@@ -31,20 +31,18 @@ if(in_array('woocommerce/woocommerce.php', apply_filters('active_plugins', get_o
     remove_action('woocommerce_archive_description', 'woocommerce_taxonomy_archive_description', 10);
     remove_action('woocommerce_archive_description', 'woocommerce_product_archive_description', 10);
 
-    //Переносим уведомление выше (woocommerce_result_count hook)
+    // Переносим уведомление выше (woocommerce_result_count hook)
     remove_action('woocommerce_before_shop_loop','woocommerce_result_count',20);
     add_action('woocommerce_archive_description','woocommerce_result_count',10);
 
-    //Удаление Хлебных Крошек
+    // Удаление Хлебных Крошек
     remove_action('woocommerce_before_main_content','woocommerce_breadcrumb',20);
 
-    //Перенос сортировки Страницы
+    // Перенос сортировки Страницы
     remove_action('woocommerce_before_shop_loop','woocommerce_catalog_ordering',30);
 
-    //Карточка Продукта, Product Image
-
-       //Create new hook to wrap product image in <div> tag and add custom class
-       //add <div> wrapper with class products__img
+    /* Карточка Продукта, Product Image */
+    // Add <div> wrapper
     add_action('woocommerce_before_shop_loop_item','lege_wrapforimage_open', 5); //open wrapper
     add_action('woocommerce_before_shop_loop_item_title','lege_wrapforimage_close', 20); //close wrapper
     function lege_wrapforimage_open(){
@@ -53,15 +51,12 @@ if(in_array('woocommerce/woocommerce.php', apply_filters('active_plugins', get_o
     function lege_wrapforimage_close(){
         echo '</div>';
     }
-
     
-        //wrap <a> tag around product image
+    // Wrap <a> tag around product image
     add_action('woocommerce_before_shop_loop_item_title','woocommerce_template_loop_product_link_close', 15);
     remove_action ('woocommerce_after_shop_loop_item','woocommerce_template_loop_product_link_close', 5);
  
-
-    //wrap in <div> tag and add custom class to product item
-    //add <div> wrapper with class products__bottom
+    // Add <div> wrappers
     add_action('woocommerce_shop_loop_item_title',function(){
         echo '<div class="products__bottom">';
     },5);
@@ -69,7 +64,7 @@ if(in_array('woocommerce/woocommerce.php', apply_filters('active_plugins', get_o
         echo '</div>';
     },15);
 
-    //add <div> wrapper with class products__detail
+    // Add <div> wrapper
     remove_action('woocommerce_shop_loop_item_title','woocommerce_template_loop_product_title',10);
     add_action('woocommerce_shop_loop_item_title','lege_product_data',10);
     function lege_product_data(){
@@ -79,27 +74,15 @@ if(in_array('woocommerce/woocommerce.php', apply_filters('active_plugins', get_o
         $title = get_the_title();
         ?>
 
-        <!--
         <div class="products__detail">
-           <a href="<?php //the_permalink(); ?>" class="products__name"><?php //the_title(); ?></a>
-           <div class="price">
-                <div class="price__old"><span class="currency">$</span>89</div>
-                <div class="price__now"><span class="currency">$</span>67</div>
-           </div>
-           <div class="rate"></div> 
-        </div>
-        -->
-        <div class="products__detail">
-            <a href="<?php the_permalink(); ?>" class="products__name"><?php
+            <a href="<?php the_permalink(); ?>" class="products__name">
+                <?php
                 if(get_post_meta(get_the_ID(),'lege_short_title',true)){
                     echo get_post_meta(get_the_ID(),'lege_short_title',true);
                 } else {
-                    /* echo $title; 
-                    echo strlen($title) > 17 ? substr($title, 0, 17) : $title; */
                     echo mb_substr( get_the_title(), 0, 17, 'UTF-8' );
-                    
-                    /* echo wp_trim_words( get_the_title(), 3, '...' ); //trim by words */
-                }  ?>
+                }  
+                ?>
                 </a>
             <div class="price">
                 <?php echo $product->get_price_html(); ?>
@@ -109,13 +92,15 @@ if(in_array('woocommerce/woocommerce.php', apply_filters('active_plugins', get_o
 
     <?php }
 
-        //remove rating and price
+    // Remove rating and price
     remove_action('woocommerce_after_shop_loop_item_title','woocommerce_template_loop_rating',5);
     remove_action('woocommerce_after_shop_loop_item_title','woocommerce_template_loop_price',10);
 
 
-//Sale 
-    //display the “HOT / NEW” badge
+    /* Sale */
+
+    // Display the “HOT / NEW” badge
+    /*
     function lege_show_status(){
         if(get_post_meta(get_the_ID(),'lege_sale_button_title',true)){
             $color = '';
@@ -126,10 +111,29 @@ if(in_array('woocommerce/woocommerce.php', apply_filters('active_plugins', get_o
         }
     }
     add_action('woocommerce_before_shop_loop_item', 'lege_show_status', 9);
-    //hook to single product page
+    // Hook to single product page
     add_action('woocommerce_before_single_product_summary', 'lege_show_status', 5);
+*/
+function lege_show_status() {
+    global $product;
 
-    //show sale price at cart
+    if ( ! $product ) {
+        return;
+    }
+
+    $product_id = $product->get_id();
+    $title = get_post_meta( $product_id, 'lege_sale_button_title', true );
+    $color = get_post_meta( $product_id, 'lege_sale_button_color', true );
+
+    if ( ! empty( $title ) ) {
+        $style = $color ? 'style="background:' . esc_attr( $color ) . ';"' : '';
+        echo '<span class="new-item" ' . $style . '>' . esc_html( $title ) . '</span>';
+    }
+}
+add_action( 'woocommerce_before_shop_loop_item', 'lege_show_status', 9 );
+//add_action( 'woocommerce_before_single_product_summary', 'lege_show_status', 5 );
+
+    // Show sale price at cart
     function my_custom_show_sale_price_at_cart( $old_display, $cart_item, $cart_item_key ) {
         /** @var WC_Product $product */
         $product = $cart_item['data'];
@@ -141,9 +145,9 @@ if(in_array('woocommerce/woocommerce.php', apply_filters('active_plugins', get_o
     add_filter('woocommerce_cart_item_price', 'my_custom_show_sale_price_at_cart', 10, 3 );
 
 
-//Открытый товар - single product page
+    /* Открытый товар - Single product page */
 
-    //sku and in stock()
+    // Sku and in stock()
     remove_action('woocommerce_single_product_summary', 'woocommerce_template_single_meta', 40); 
     add_action('woocommerce_single_product_summary', 'lege_woo_sku_custom', 4);
 
@@ -166,23 +170,20 @@ if(in_array('woocommerce/woocommerce.php', apply_filters('active_plugins', get_o
     <?php
     }
 
-    //meta 
-    //remove_action('woocommerce_single_product_summary', 'woocommerce_template_single_meta', 40);
-
-    //remove woo's stock output completetly from single product, <p class="stock in-stock">…</p> which comes from wc_get_stock_html()
+    // Remove woo's stock output completetly from single product
     add_filter('woocommerce_get_stock_html', function($html, $product) {
         if( is_product() ) {
             return ''; //don't print stock
         } else {
-            return $html; //print stock on archive, cart, etc.
+            return $html; //print stock on archive, cart
         }
     }, 10, 2);
 
-    //title 
+    // Title 
     remove_action('woocommerce_single_product_summary', 'woocommerce_template_single_title', 5);
     add_action('woocommerce_single_product_summary', 'lege_template_single_title', 5); 
 
-    //excerpt - remove description cpmletele
+    // Excerpt - remove description cpmletele
     remove_action('woocommerce_single_product_summary', 'woocommerce_template_single_excerpt', 20);
     remove_action('woocommerce_single_product_summary', 'woocommerce_template_single_excerpt', 6);
 
@@ -197,8 +198,7 @@ if(in_array('woocommerce/woocommerce.php', apply_filters('active_plugins', get_o
         }
     }
 
-
-    //price
+    // Price
     remove_action('woocommerce_single_product_summary', 'woocommerce_template_single_price', 10);
     add_action('woocommerce_before_add_to_cart_button', 'lege_custom_addtocart_price', 5);
     function lege_custom_addtocart_price() {
@@ -209,89 +209,59 @@ if(in_array('woocommerce/woocommerce.php', apply_filters('active_plugins', get_o
 
         <?php }
 
+    // Share Icons on single product page
+    add_action('woocommerce_share', 'lege_custom_share', 5);
+    function lege_custom_share() { 
+        ?>
+        <div class="share">
+        <p class="share__title">
+            Поделиться:
+        </p>
+        <ul class="social">
+            <li class="social__item">
+                <span>Vk</span>
+                <a data-social="vkontakte" onclick="window.open(this.href, 'Share on VK', 'width=600,height=300'); return false" class="social__icon social__icon_vk" href="<?php echo lege_get_share('vk', get_the_permalink(), get_the_title()); ?>">
+                    <svg  width="21" height="18">
+                        <use xlink:href="#vk"/>
+                    </svg>
+                </a>
+            </li>
+            <li class="social__item">
+                <span>Fb</span>
+                <a data-social="facebook" onclick="window.open(this.href, 'Share on Facebook', 'width=600,height=300'); return false" class="social__icon social__icon_fb" href="<?php echo lege_get_share('fb', get_the_permalink(), get_the_title()); ?>">
+                    <svg  width="14" height="17">
+                        <use xlink:href="#facebook"/>
+                    </svg>
+                </a>
+            </li>
+            <li class="social__item">
+                <span>Tw</span>
+                <a data-social="twitter" onclick="window.open(this.href, 'Share on Twitter', 'width=600,height=300'); return false" class="social__icon social__icon_tw" href="<?php echo lege_get_share('twi', get_the_permalink(), get_the_title()); ?>">
+                    <svg  width="18" height="15">
+                        <use xlink:href="#twitter"/>
+                    </svg>
+                </a>
+            </li>
+        </ul>
+        </div>
+    
+    <?php }
 
-    //Share Icons on single product page
-
-        //share.php проверка, где отображать 
-        /*
-            add_action('woocommerce_share', function() {
-                echo 'test';
-            });
-        */
-
-        add_action('woocommerce_share', 'lege_custom_share', 5);
-        function lege_custom_share() { 
-            //check if gets correct permalink
-            //echo get_permalink();
-            
-            ?>
-            <div class="share">
-            <p class="share__title">
-                Поделиться:
-            </p>
-            <ul class="social">
-                <li class="social__item">
-                    <span>Vk</span>
-                    <a data-social="vkontakte" onclick="window.open(this.href, 'Share on VK', 'width=600,height=300'); return false" class="social__icon social__icon_vk" href="<?php echo lege_get_share('vk', get_the_permalink(), get_the_title()); ?>">
-                        <svg  width="21" height="18">
-                            <use xlink:href="#vk"/>
-                        </svg>
-                    </a>
-                </li>
-                <li class="social__item">
-                    <span>Fb</span>
-                    <a data-social="facebook" onclick="window.open(this.href, 'Share on Facebook', 'width=600,height=300'); return false" class="social__icon social__icon_fb" href="<?php echo lege_get_share('fb', get_the_permalink(), get_the_title()); ?>">
-                        <svg  width="14" height="17">
-                            <use xlink:href="#facebook"/>
-                        </svg>
-                    </a>
-                </li>
-                <li class="social__item">
-                    <span>Tw</span>
-                    <a data-social="twitter" onclick="window.open(this.href, 'Share on Twitter', 'width=600,height=300'); return false" class="social__icon social__icon_tw" href="<?php lege_get_share('twi', get_the_permalink(), get_the_title()); ?>">
-                        <svg  width="18" height="15">
-                            <use xlink:href="#twitter"/>
-                        </svg>
-                    </a>
-                </li>
-            </ul>
-            </div>
-        
-        <?php }
-
-    //Image Slider for Single Product Page 
-    /*
-    add_action( 'after_setup_theme', 'lege_woocommerce_plugin_setup'); 
-            function lege_woocommerce_plugin_setup() {
-                add_theme_support( 'wc-product-gallery-zoom' );
-                add_theme_support( 'wc-product-gallery-lightbox' );
-                add_theme_support( 'wc-product-gallery-slider' );
-            } 
-    */
-
-    //Change Add to Cart button text on single product page in popup modal window
+    // Change Add to Cart button text on single product page in popup modal window
     add_filter( 'woocommerce_product_single_add_to_cart_text', function( $text ) {
-        return __( 'Перейти в корзину', 'lege_add_to_cart_button_text' );
+        return __( 'Перейти в корзину', 'lege' );
     });     
 
+    // Remove default avatar
+    remove_action('woocommerce_review_before', 'woocommerce_review_display_gravatar', 10);
 
-
-// Remove Upsell and Related Products on Single Product
-//remove_action('woocommerce_after_single_product_summary', 'woocommerce_upsell_display', 15);
-//remove_action('woocommerce_after_single_product_summary', 'woocommerce_output_related_products', 20);
-
-//Remove default avatar
-remove_action('woocommerce_review_before', 'woocommerce_review_display_gravatar', 10);
-
-
-
-//Comment Form on Single Product Page
+    /* Comment Form on Single Product Page */
     function my_custom_comment_fields( $fields ) {
         $commenter = wp_get_current_commenter();
         $req = (bool) get_option( 'require_name_email', 1 );
         $aria_req = ( $req ? " aria-required='true'" : '' );
         
-        // Custom field template with <div> wrappers and custom classes
+        // Custom field template with <div> wrappers
         $custom_field_template = function( $field_html, $key, $field_data ) use ( $commenter, $req, $aria_req ) {
             $value = esc_attr( $commenter[ 'comment_author_' . $key ] );
             $required_html = ( $field_data['required'] ? '&nbsp;<span class="required">*</span>' : '' );
@@ -349,8 +319,7 @@ remove_action('woocommerce_review_before', 'woocommerce_review_display_gravatar'
         }
     }
 
-    // Show phone number, only to admins 
-    // Only validate for non-admins
+    // Show phone number, only to admins (Only validate for non-admins)
     add_filter('preprocess_comment', 'lege_validate_comment_fields');
     function lege_validate_comment_fields($commentdata) {
         if ( ! current_user_can('manage_options') ) {
@@ -372,7 +341,7 @@ remove_action('woocommerce_review_before', 'woocommerce_review_display_gravatar'
         return $comment_text;
     }
 
-    /* Move comment field to bottom */
+    // Move comment field to bottom
     function lege_move_comment_field_to_bottom($fields) {
         $comment_field = $fields['comment'];
         unset($fields['comment']);
@@ -381,7 +350,7 @@ remove_action('woocommerce_review_before', 'woocommerce_review_display_gravatar'
     }
     add_filter('comment_form_fields', 'lege_move_comment_field_to_bottom');
  
-    /* Remove the default cookies field */
+    // Remove the default cookies field
     function remove_comment_cookies_field( $fields ) {
     if ( isset( $fields['cookies'] ) ) {
         unset( $fields['cookies'] );
@@ -390,9 +359,9 @@ remove_action('woocommerce_review_before', 'woocommerce_review_display_gravatar'
     }
     add_filter( 'comment_form_fields', 'remove_comment_cookies_field' );
 
-    
-/* Cart */ 
-    //move cross sell - cart page */ 
+    /* Cart */ 
+
+    // Move cross sell - cart page */ 
     remove_action('woocommerce_cart_collaterals', 'woocommerce_cross_sell_display');
     add_action('woocommerce_after_cart', 'display_cross_sells_under_cart_results');
     function display_cross_sells_under_cart_results() {
@@ -401,13 +370,8 @@ remove_action('woocommerce_review_before', 'woocommerce_review_display_gravatar'
         echo '</div>';
     }
 
-
-/* Checkout */
+    /* Checkout */
     remove_action( 'woocommerce_checkout_terms_and_conditions', 'wc_checkout_privacy_policy_text', 20);
-
-
-
-
 
 
 }
--- themes/lege/woocommerce/loop/add-to-cart.php
+++ themes/lege/woocommerce/loop/add-to-cart.php
@@ -10,10 +10,9 @@
  * happen. When this occurs the version of the template file will be bumped and
  * the readme will list any important changes.
  *
- * @see 	    https://docs.woocommerce.com/document/template-structure/
- * @author 		WooThemes
- * @package 	WooCommerce/Templates
- * @version     3.3.0
+ * @see         https://woocommerce.com/document/template-structure/
+ * @package     WooCommerce\Templates
+ * @version     9.2.0
  */
 
 if ( ! defined( 'ABSPATH' ) ) {
@@ -22,31 +21,23 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 global $product;
 
-//print_r($args['class']);
-
-
-// WITH AJAX
-/*
-echo apply_filters( 'woocommerce_loop_add_to_cart_link', // WPCS: XSS ok.
-	sprintf( '<a href="%s" data-quantity="%s" class="%s" %s>%s</a>',
+echo apply_filters(
+	'woocommerce_loop_add_to_cart_link',
+	sprintf(
+		'<a href="%s" %s data-quantity="%s" class="%s" %s>%s</a>',
 		esc_url( $product->add_to_cart_url() ),
+		$aria_describedby,
 		esc_attr( isset( $args['quantity'] ) ? $args['quantity'] : 1 ),
-		'product_type_simple add_to_cart_button ajax_add_to_cart products__but',
-		isset( $args['attributes'] ) ? wc_implode_html_attributes( $args['attributes'] ) : '',
-		''
-	),
-$product, $args );
-*/
-
-
-// NO AJAX (loads all the product archive page)
-echo apply_filters( 'woocommerce_loop_add_to_cart_link', // WPCS: XSS ok.
-	sprintf( '<a href="%s" data-quantity="%s" class="%s" %s>%s</a>',
-		esc_url( $product->add_to_cart_url() ),
-		esc_attr( isset( $args['quantity'] ) ? $args['quantity'] : 1 ),
-		// class removed -> ajax_add_to_cart 
 		'product_type_simple add_to_cart_button products__but',
 		isset( $args['attributes'] ) ? wc_implode_html_attributes( $args['attributes'] ) : '',
-		''
+		esc_html__( '' )
 	),
-$product, $args );
+	$product,
+	$args
+);
+?>
+<?php if ( isset( $args['aria-describedby_text'] ) ) : ?>
+	<span id="woocommerce_loop_add_to_cart_link_describedby_<?php echo esc_attr( $product->get_id() ); ?>" class="screen-reader-text">
+		<?php echo esc_html( $args['aria-describedby_text'] ); ?>
+	</span>
+<?php endif; ?>
