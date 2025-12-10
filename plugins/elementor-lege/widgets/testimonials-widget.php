70491c975218fc2eeb37857fb11879abdb78890e
Elina Ontisvili <63250296+elinaontisvili@users.noreply.github.com>
2025-12-10 12:32:31 +0200

    feat(testimonials-widget): add style controls for title, subtitle, name, text, image size, and spacing

--- plugins/elementor-lege/widgets/testimonials-widget.php
+++ plugins/elementor-lege/widgets/testimonials-widget.php
@@ -89,6 +89,150 @@ class Elementor_Testimonials_Widget extends \Elementor\Widget_Base {
         );
 
         $this->end_controls_section();
+
+        // Style tab 
+        $this->start_controls_section(
+            'style_section',
+            [
+                'label' => __('Styles', 'elementor-lege'),
+                'tab'   => \Elementor\Controls_Manager::TAB_STYLE,
+            ]
+        );
+
+        // Main title
+        $this->add_control(
+            'title_color',
+            [
+                'label' => __('Title Color', 'elementor-lege'),
+                'type'  => \Elementor\Controls_Manager::COLOR,
+                'selectors' => [
+                    '{{WRAPPER}} .clients__title' => 'color: {{VALUE}};',
+                ],
+            ]
+        );
+
+        $this->add_group_control(
+            \Elementor\Group_Control_Typography::get_type(),
+            [
+                'name' => 'title_typography',
+                'label' => __('Title Typography', 'elementor-lege'),
+                'selector' => '{{WRAPPER}} .clients__title',
+            ]
+        );
+
+        // Title span
+        $this->add_control(
+            'title_span_color',
+            [
+                'label' => __('Subtitle Color', 'elementor-lege'),
+                'type'  => \Elementor\Controls_Manager::COLOR,
+                'selectors' => [
+                    '{{WRAPPER}} .clients__title span' => 'color: {{VALUE}};',
+                ],
+            ]
+        );
+
+        $this->add_group_control(
+            \Elementor\Group_Control_Typography::get_type(),
+            [
+                'name' => 'subtitle_typography',
+                'label' => __('Subtitle Typography', 'elementor-lege'),
+                'selector' => '{{WRAPPER}} .clients__title span',
+            ]
+        );
+
+        // Name
+        $this->add_control(
+            'name_color',
+            [
+                'label' => __('Name Color', 'elementor-lege'),
+                'type'  => \Elementor\Controls_Manager::COLOR,
+                'selectors' => [
+                    '{{WRAPPER}} .clients__name' => 'color: {{VALUE}};',
+                ],
+            ]
+        );
+
+        $this->add_group_control(
+            \Elementor\Group_Control_Typography::get_type(),
+            [
+                'name' => 'name_typography',
+                'label' => __('Name Typography', 'elementor-lege'),
+                'selector' => '{{WRAPPER}} .clients__name',
+            ]
+        );
+
+        $this->add_control(
+            'text_color',
+            [
+                'label' => __('Text Color', 'elementor-lege'),
+                'type'  => \Elementor\Controls_Manager::COLOR,
+                'selectors' => [
+                    '{{WRAPPER}} .clients__text' => 'color: {{VALUE}};',
+                ],
+            ]
+        );
+
+        $this->add_group_control(
+            \Elementor\Group_Control_Typography::get_type(),
+            [
+                'name' => 'text_typography',
+                'label' => __('Text Typography', 'elementor-lege'),
+                'selector' => '{{WRAPPER}} .clients__text',
+            ]
+        );
+
+        $this->add_control(
+            'image_size',
+            [
+                'label' => __('Image Size', 'elementor-lege'),
+                'type' => \Elementor\Controls_Manager::SLIDER,
+                'size_units' => [ 'px', '%' ],
+                'range' => [
+                    'px' => [
+                        'min' => 40,
+                        'max' => 300,
+                    ],
+                ],
+                'default' => [
+                    'unit' => 'px',
+                    'size' => 120,
+                ],
+                'selectors' => [
+                    '{{WRAPPER}} .clients__img img' => 'width: {{SIZE}}{{UNIT}}; height: {{SIZE}}{{UNIT}};',
+                ],
+            ]
+        );
+
+        $this->add_control(
+            'box_padding',
+            [
+                'label' => __('Box Padding', 'elementor-lege'),
+                'type' => \Elementor\Controls_Manager::DIMENSIONS,
+                'size_units' => [ 'px', 'em', '%' ],
+                'selectors' => [
+                    '{{WRAPPER}} .clients__box' => 'padding: {{TOP}}{{UNIT}} {{RIGHT}}{{UNIT}} {{BOTTOM}}{{UNIT}} {{LEFT}}{{UNIT}};',
+                ],
+            ]
+        );
+
+        $this->add_control(
+            'slide_spacing',
+            [
+                'label' => __('Spacing Between Slides', 'elementor-lege'),
+                'type' => \Elementor\Controls_Manager::SLIDER,
+                'range' => [
+                    'px' => ['min' => 0, 'max' => 80],
+                ],
+                'default' => ['size' => 20],
+                'selectors' => [
+                    '{{WRAPPER}} .clients__slide' => 'margin-right: {{SIZE}}{{UNIT}};',
+                ],
+            ]
+        );
+
+        $this->end_controls_section();
+
     }
 
     protected function render(): void {
@@ -105,10 +249,10 @@ class Elementor_Testimonials_Widget extends \Elementor\Widget_Base {
     $prefix = "lege_";
 
     ?>
-    <section class="clients">
+    <section class="clients" aria-labelledby="clients-title">
         <div class="wrapper">
 
-            <h2 class="clients__title secondary-title">
+            <h2 id="clients-title" class="clients__title secondary-title">
                 <span><?php echo esc_html($s['title_span']); ?></span><br>
                 <?php echo esc_html($s['title_main']); ?>
             </h2>
@@ -125,7 +269,7 @@ class Elementor_Testimonials_Widget extends \Elementor\Widget_Base {
 
                         // Fetch custom metabox data start
                         $social_link = '';
-                        $display_date = get_the_date('d.m.Y'); // Default to post creation date
+                        $display_date = get_the_date('d.m.Y');
 
                         if ($is_testimonial_cpt) {
                             $social_link = get_post_meta(get_the_ID(), $prefix . 'social_link', true);
@@ -152,7 +296,7 @@ class Elementor_Testimonials_Widget extends \Elementor\Widget_Base {
 
                         $image = get_the_post_thumbnail_url(get_the_ID(), 'large');
                         if (!$image) {
-                            $image = \Elementor\Utils::get_placeholder_image_src(); // Better Elementor fallback
+                            $image = \Elementor\Utils::get_placeholder_image_src();
                         }
                         ?>
 
@@ -162,7 +306,7 @@ class Elementor_Testimonials_Widget extends \Elementor\Widget_Base {
                                 <div class="clients__photo">
                                     <div class="clients__img">
                                         <img src="<?php echo esc_url($image); ?>"
-                                             alt="<?php echo esc_attr(get_the_title()); ?>">
+                                            alt="Photo of <?php echo esc_attr(get_the_title()); ?>">
                                     </div>
 
                                     <?php if (!empty($social_link) || $is_testimonial_cpt): ?>
@@ -178,15 +322,8 @@ class Elementor_Testimonials_Widget extends \Elementor\Widget_Base {
                                     <p class="clients__name"><?php the_title(); ?></p>
 
                                     <div class="clients__text">
-                                        <?php /* echo apply_filters('the_content', get_the_content()); */ ?>
-
-                                        <?php /* Trim content
-                                        $content = get_the_content();
-                                        $trimmed = wp_trim_words( $content, 130, '...' );
-                                        echo apply_filters('the_content', $trimmed);
-                                        */ ?>
-
-                                        <?php // Metabox for testimonial content for the slider
+  
+                                        <?php
                                         $frontpage_testimonial = get_post_meta(get_the_ID(), $prefix . 'testy_frontpage', true);
 
                                         if ( !empty($frontpage_testimonial) ) {
@@ -219,6 +356,58 @@ class Elementor_Testimonials_Widget extends \Elementor\Widget_Base {
         </div>
     </section>
     <?php
+    }
+
+    protected function content_template(): void {
+
+        ?> 
+
+        <!-- Clients -->
+		<section class="clients">
+			<div class="wrapper">
+                <h2 class="clients__title secondary-title">
+                    <span>{{{ settings.title_span }}}</span><br>
+                    {{{ settings.title_main }}}
+                </h2>
+
+				<div class="clients__slider">
+
+                <# for ( let i = 0; i < settings.posts_per_page; i++ ) { #>
+					<div class="clients__slide">
+						<div class="clients__box">
+							<div class="clients__photo">
+								<div class="clients__img">
+                                    <img src="img/client1.jpg" alt="Анна Свиридова">
+								</div>
+								<a href="#" class="clients__link">
+									<svg  width="14" height="17">
+										<use xlink:href="#facebook"/>
+									</svg>
+								</a>
+							</div>
+							<div class="clients__say">
+								
+                                <p class="clients__name">Анна Свиридова</p>
+
+								<div class="clients__text">
+									<p>Наша компания ведет свою деятельность в сфере ИТ-бизнеса, которая на сегодняшний день является специфической областью для правового регулирования. И, несмотря на это, в процессе сотрудничества с нами работники юридической компании JC показали высокий уровень профессионализма и способность оперативно решать любые поставленные задачи.</p>
+									<p>Нашей организации был предоставлен обширный перечень юридических услуг, таких как устные и письменные консультации; экспертиза документов и деятельности нашей компании; представление наших интересов перед контрагентами; составление различных юридических документов и т.д. Данные услуги были оказаны своевременно и качественно. </p>
+								</div>
+							</div>
+							<div class="add-time">
+								<svg width="13" height="13">
+									<use xlink:href="#time"/>
+								</svg>
+								<p class="add-time__date">20.05.2018</p>
+							</div>
+						</div>
+					</div>
+                <# } #>
+				</div>
+			</div>
+		</section><!-- End clients -->
+
+    <?php 
     }
 }
 ?>
