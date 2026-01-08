6c2de5dce99b1ac9e9508d746549b7a42d15843a
Elina Ontisvili <63250296+elinaontisvili@users.noreply.github.com>
2026-01-08 11:54:34 +0200

    fix(widget-customcategory): replace Polylang translation function with standard translation for widget title

--- themes/lege/inc/widgets/widget-customcategory.php
+++ themes/lege/inc/widgets/widget-customcategory.php
@@ -32,7 +32,7 @@ class Lege_Category_Widget extends WP_Widget
     {
         extract( $args );
 
-        $title = pll__( $instance['title'] );
+        $title = __( $instance['title'] );
 
         // Display Widget
         ?>
@@ -76,11 +76,6 @@ class Lege_Category_Widget extends WP_Widget
         $instance = $old_instance;
 
         $instance['title'] = strip_tags( $new_instance['title'] );
-
-        // Register strings for Polylang 
-        if ( function_exists( 'pll_register_string' ) ) {
-            pll_register_string( 'Categories Widget Title', $instance['title'], 'Widgets' );
-        } 
 
         return $instance;
     }
