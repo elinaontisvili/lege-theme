56f7a04995da1d1f3ff9df8cd83ebd3db39e4e62
Elina Ontisvili <63250296+elinaontisvili@users.noreply.github.com>
2026-01-08 12:05:50 +0200

    fix(widget-rating): replace Polylang translation function with standard translation for widget title

--- themes/lege/inc/widgets/widget-rating.php
+++ themes/lege/inc/widgets/widget-rating.php
@@ -75,7 +75,7 @@ class Lege_Rating_Widget extends WP_Widget
     {
         extract( $args );
 
-        $title = pll__( $instance['title'] );
+        $title = __( $instance['title'] );
         ob_start();
 
         $found = false;
@@ -127,11 +127,6 @@ class Lege_Rating_Widget extends WP_Widget
         $instance = $old_instance;
 
         $instance['title'] = strip_tags( $new_instance['title'] );
-
-        // Register strings for Polylang
-        if ( function_exists( 'pll_register_string' ) ) {
-            pll_register_string( 'Rating Widget Title', $instance['title'], 'Widgets' );
-        }
 
         return $instance;
     }
